b883cf4d006ff87b48c79112ac3af175999b7351
fahedarshad01 <arshad.fahed@gmail.com>
2021-08-09 17:36:10 +0100

    my first basic website!

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/background1.png") {
+        myImage.setAttribute('src','images/background4.png');
+    } else {
+        myImage.setAttribute('src', 'images/background1.png');
+    }
+        
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if (myName === null || !(Boolean(myName))) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'You are a muslim, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = "You are a muslim, " + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
